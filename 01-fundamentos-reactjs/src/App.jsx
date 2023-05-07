a75dea83742c0ea4cd9d8f6162a2ca82bb7cdb9d
marvim1990 <marcosvinim.cruz@gmail.com>
2023-05-07 06:19:07 -0300

    initial commit

---
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Post from './Post'
+
+function App() {
+  return (
+    <>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </>
+  )
+}
+
+export default App
